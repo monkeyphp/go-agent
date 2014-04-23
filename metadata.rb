98e386494af874ea6ecc4eb3310b397f9d2f8601
David White <david@monkeyphp.com>
2014-04-23 19:51:25 +0100

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'go-agent'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures go-agent'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
